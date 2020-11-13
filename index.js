7d4f5450449b96926d00cec31fa84991f40f3f72
don Pablo <don@obeyi.com>
2020-11-13 13:23:27 +0200

    Signed-off-by: don Pablo <don@obeyi.com>

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 module.exports = Franz => Franz;
+
+module.exports = (Franz) =>
+  class Deezer extends Franz {
+    modifyRequestHeaders() {
+      return [
+        {
+          headers: {
+            'user-agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36",
+          },
+          requestFilters: {
+            urls: ['*://*/*'],
+          }
+        }
+      ]
+    }
+  };
